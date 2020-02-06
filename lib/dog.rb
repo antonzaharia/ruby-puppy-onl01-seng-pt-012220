2452c809c8945712a9f48cbc10ac4115ca604040
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-02-06 15:24:53 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,13 +5,15 @@ class Dog
   
   def initialize(name)
     @name = name
-    @@all << self
   end
   def self.all
     @@all
   end
   def self.print_all
     @@all.each {|dog| puts dog.name}
+  end
+  def self.save
+    @@all << self
   end
   
 end
